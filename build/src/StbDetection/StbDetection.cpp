52fd21fb38b06ad9a013a72cf99117349d092d7b
Dima73 <Dima-73@inbox.lv>
2017-07-12 21:34:16 +0300

    Revert "Fix register offset Ultimo 4k (thanks Rockman)"
    This reverts commit 8830a619db787560014de76f6783e56ce2e78c90.

--- build/src/StbDetection/StbDetection.cpp
+++ build/src/StbDetection/StbDetection.cpp
@@ -297,15 +297,10 @@ bool CStb::DetectSTB()
 		case BRCM7376:
 		case BRCM7251:
 		case BRCM7252:
+		case BRCM7444:
 			registeroffset = 0xf0600000;
 			chr_luma_stride = 0x80;
 			chr_luma_register_offset = 0x3c;
-			mem2memdma_register = 0;
-			break;
-		case BRCM7444:
-			registeroffset = 0x10600000;
-			chr_luma_stride = 0x40;
-			chr_luma_register_offset = 0x34;
 			mem2memdma_register = 0;
 			break;
 		default:
